da19f7deed21d57e8dea6cf100e16e2a6f43fa84
Shruti <katdareshruti@yahoo.com>
2020-01-11 20:54:47 -0800

    Added Access-Control-Allow-Credentials to app.js

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     next();
